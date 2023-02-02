0a88f205b0b916a3969bb69525da8dbd9b55cf36
rashadheydarli <rashadahe@code.edu.az>
2023-02-02 16:52:47 +0400

    task added

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace extensionmethod
+{
+    public class Program
+    {
+        static void Main()
+        {
+            var user1 = new User();
+            user1.Name ="Rashad";
+            user1.Surname = "Heydarli";
+            user1.RegistrationMonth = RegistrationMonth.September;
+            user1.PIN = 3D83JX6;
+            Console.WriteLine(user1.GetDetail()); 
+
+
+
+
+
+
+
+        }
+    }
+}
+
+
---
+++ User.cs
@@ -0,0 +1,56 @@
+﻿using System;
+namespace extensionmethod
+{
+	public class User
+	{
+		public int pin;
+
+		public string Name { get; set; }
+		public string Surname { get; set; }
+		public RegistrationMonth RegistrationMonth { get; set; }
+		public int PIN
+		{
+			get
+			{
+				return pin;
+			}
+			set
+			{ 
+				if (value.Length == 7)
+				{
+					pin = value;
+				}
+				else
+				{
+					throw new Exception("Duzgun deyil");
+				}
+			}
+		}
+		public User( int pin)
+		{
+			this.pin = pin;
+		}
+        public static string GetDetail(this int pin,string name, string surname, int registrationmonth )
+        {
+            return $"Name: {name}\n Surname: {surname}\n Reg.Month: {registrationmonth}\n Pin: {pin}";
+
+        }
+    }
+
+	enum RegistrationMonth
+	{
+			January=1,
+			February, 
+			March,
+			April, 
+			May ,
+			June ,
+			July ,
+			August,
+			September, 
+			October ,
+			November ,
+			December 
+    }
+}
+
